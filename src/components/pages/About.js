5e4bcccedbf02c4a04c88ed2d33ca9a578559777
kpetrosky <kpetrosky36@gmail.com>
2023-06-21 09:42:37 -0700

    trying to add background to css, also got all pictures workong on portfolio, also made font a little big, added about me info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import backgroundImage1 from '../../images/background/background.jpg'
 
 export default function About() {
   const divStyle = {
@@ -13,17 +14,29 @@ export default function About() {
       <div style={divStyle}>
         <h1>About Page</h1>
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          Hello, I’m Krystal Petrosky, a passionate Full Stack engineer with a love for creating engaging and robust applications. While I am relatively new to the Full Stack community, I have developed a strong foundation in various technologies such as React, SQL, MERN, HTML/CSS, JavaScript, Node.js, ORM, MVC, and many others. I thrive on challenges and enjoy finding innovative solutions to complex problems.
+        </p>
+
+        <p>
+          Throughout my career, I have worked on a diverse range of projects, each contributing to my growth and expanding my skill set. As a lead 1st-grade teacher, I not only guided peers who were starting out as teachers but also fostered a growth mindset in young minds. During my Full Stack Bootcamp, I collaborated on team projects that aimed to create exceptional user experiences. I have also worked on individual projects, honing my skills and resourcefulness to deliver high-quality results consistently.
+        </p>
+
+        <p>
+          I excel in problem-solving, a skill that has been invaluable in my previous teaching career as well as in coding. My attention to detail, strong problem-solving abilities, and dedication to continuous learning enable me to adapt quickly to new technologies and industry trends. I am always eager to expand my knowledge and stay ahead in this dynamic field.
+        </p>
+
+        <p>
+          In addition to my technical skills, I am a collaborative team player. While I can take charge when needed, I value open communication, mutual respect, and the exchange of ideas to foster creativity and achieve project success. I believe that collaboration and teamwork are essential for bringing ideas to life.
+        </p>
+
+        <p>
+          Beyond work, I am an avid reader, particularly enjoying romance stories with a satisfying happily ever after. I am also working on writing my own novel, which challenges me to persevere and complete ambitious projects. Additionally, I am passionate about web development and love exploring new technologies and frameworks. Solving coding challenges is another interest of mine, as it helps me think critically, optimize code efficiency, and find elegant solutions to complex problems, all of which are crucial for success as a Full Stack Developer.
+        </p>
+
+        <p>
+          Thank you for taking the time to explore my portfolio. I am thrilled to share my projects and experiences with you. If you have any inquiries or collaboration opportunities, please feel free to reach out. Let’s create something amazing together!
         </p>
       </div>
     </Container>
   );
 }
-
-
-
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,10 @@ import apiQuiz from '../../images/apiQuiz.png';
 import mod5 from '../../images/mod 5.png';
 import mvc from '../../images/mvc.png';
 import express from '../../images/express.png'
+import sql from '../../images/sql.png'
+import pwa from '../../images/pwa.png'
+
+
 export default function Portfolio() {
   const projects = [
     {
@@ -13,7 +17,7 @@ export default function Portfolio() {
       description: true,
       Repo: "https://github.com/AlexWatabe9/DailyDistractions",
       deployed: 'https://alexwatabe9.github.io/DailyDistractions/',
-      src: project1 ,
+      src: project1,
       backgroundColor: '#e5446dff',
     },
     {
@@ -22,20 +26,11 @@ export default function Portfolio() {
       description: true,
       Repo: "https://github.com/kpetrosky/SQL-Challenge-Employee-Tracker",
       deployed: 'https://watch.screencastify.com/v/m3nkzd9RcgqnyzR2tkQx',
-      src: 'https://watch.screencastify.com/v/m3nkzd9RcgqnyzR2tkQx',
+      src: sql,
       backgroundColor: '#dee2ffff',
     },
     {
       id: 3,
-      name: "Third-Party APIs Challenge: Work Day Scheduler",
-      description: true,
-      Repo: " https://kpetrosky.github.io/Third-Party-APIs-Challenge-Work-Day-Scheduler/",
-      deployed: 'https://github.com/kpetrosky/Third-Party-APIs-Challenge-Work-Day-Scheduler',
-      src: '',
-      backgroundColor: '#82aba1ff',
-    },
-    {
-      id: 4,
       name: "Web APIs Challenge Code Quiz",
       description: true,
       Repo: "https://github.com/kpetrosky/web-apis-challenge-code-quiz",
@@ -44,7 +39,7 @@ export default function Portfolio() {
       backgroundColor: '#ff9474ff',
     },
     {
-      id: 5,
+      id: 4,
       name: "Express.js-Challenge-Note-Taker",
       description: true,
       Repo: "https://github.com/kpetrosky/Express.js-Challenge-Note-Taker",
@@ -53,21 +48,21 @@ export default function Portfolio() {
       backgroundColor: '#e8aeb7ff',
     },
     {
-      id: 6,
+      id: 5,
       name: "Progressive-Web-Applications-PWA-Challenge-Text-Editor",
       description: true,
       Repo: "https://github.com/kpetrosky/Progressive-Web-Applications-PWA-Challenge-Text-Editor",
       deployed: 'https://dashboard.heroku.com/apps/lit-sands-84115/',
-      src: '',
+      src: pwa,
       backgroundColor: '#cbc0d3ff',
     },
     {
-      id: 7,
+      id: 6,
       name: "Full stack MVC with handlebars, express, SQL",
       description: true,
       Repo: "https://github.com/kpetrosky/MVC-Model-View-Controller-MVC-Challenge-Tech-Blog",
       deployed: 'https://frozen-lake-62666.herokuapp.com/',
-      src: mvc ,
+      src: mvc,
       backgroundColor: '#a74482ff',
     },
   ];
@@ -85,10 +80,10 @@ export default function Portfolio() {
               {/* Render project details */}
               <img src={project.src} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">{project.name}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href={project.Repo} target='_blank' className="btn btn-primary me-3">Github Repo</a>
-                <a href={project.deployed} target='_blank' className="btn btn-primary">Deployed Appliction</a>
+                <h5 className="card-title" style={{ fontSize: '24px' }}>{project.name}</h5>
+                <p className="card-text" style={{ fontSize: '18px' }}>This challenge is exciting. Check it out!</p>
+                <a href={project.Repo} target="_blank" className="btn btn-primary me-3">Github Repo</a>
+                <a href={project.deployed} target="_blank" className="btn btn-primary">Deployed Application</a>
               </div>
             </div>
           </div>
