0c6e85d31d1e66f9bcbf5b5ed9060477ea93bd50
kpetrosky <kpetrosky36@gmail.com>
2023-06-23 21:43:22 -0700

    added descriptions and tools to each card

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -14,17 +14,22 @@ export default function Portfolio() {
     {
       id: 1,
       name: 'Daily Distractions',
-      description: 'The SQL Challenge: Employee Tracker is a command-line application designed to help business owners efficiently manage their company\'s employee database. Built using Node.js, Inquirer, and MySQL, this application provides an intuitive interface for viewing and organizing departments, roles, and employees within the company. As a business owner, you can easily navigate through various options to perform essential tasks such as viewing all departments, roles, and employees, adding new departments, roles, and employees, as well as updating employee roles. This empowers you to effectively organize and plan your business operations.',
+      description: 'Welcome to Daily Distractions your one stop shop to help you get away from your mundane daily tasks, or to cure your boredom. This app allows you to chose from a multitude of different topics that will all generate a random joke, number fact, and picture. Our app does this by fetching the information from multiple APIS or (Application programming interface) to randomly pull data and display them on our website.With these tools and technologies, Daily Distractions brings you an enjoyable and engaging experience, offering a brief respite from your daily routine. Explore our diverse range of topics and let our app surprise you with random jokes, intriguing number facts, and captivating pictures. It\'s time to embrace the distraction and have some fun!',
       repo: 'https://github.com/AlexWatabe9/DailyDistractions',
       deployed: 'https://alexwatabe9.github.io/DailyDistractions/',
       src: project1,
       backgroundColor: '#e5446dff',
-      toolsUsed: 'Node.js: JavaScript runtime used for executing the application. Inquirer: A command-line interface package for prompting user input and generating interactive prompts. MySQL: A popular relational database management system used for storing and managing the employee data. npm: The package manager for Node.js, used to install the necessary dependencies, such as Inquirer.'
+      toolsUsed: 'Collaboration Tools: In order to work together as a team, you will use collaboration tools such as GitHub, where you can create and manage repositories, track issues, and use project boards to organize and assign tasks.  CSS Framework: You are required to use a CSS framework other than Bootstrap. Examples of popular CSS frameworks include Bulma, Foundation, Tailwind CSS, Semantic UI, and Material-UI. The chosen CSS framework will help you style and structure your application\'s user interface efficiently. Git Branch Workflow: You will use Git for version control and collaborate with your team using a Git branch workflow. This allows you to work on different features or bug fixes in separate branches, and then merge them back into the main codebase using pull requests. GitHub Pages: The project needs to be deployed to GitHub Pages, which is a hosting service provided by GitHub. It allows you to showcase your front-end application by hosting it directly from your GitHub repository. Server-side APIs: Your application should integrate data from at least two server-side APIs. You will make requests to these APIs to fetch data and incorporate it into your application. Examples of server-side APIs include weather APIs, news APIs, social media APIs, and many others. Modals: Instead of using traditional JavaScript alerts, confirms, or prompts for user interactions, you will utilize modals. Modals provide a more elegant and customizable way to display information, receive user input, or confirm actions within your application. Client-side Storage: You will utilize client-side storage, such as local storage or session storage, to store persistent data on the user\'s browser. This allows the application to remember user preferences or save data locally for a seamless user experience.'
     },
     {
       id: 2,
       name: 'SQL Challenge Employee Tracker',
-      description: 'true',
+        description: 'The SQL Challenge: Employee Tracker is a command-line application designed to help business owners efficiently manage their company\'s employee database. Built using Node.js, Inquirer, and MySQL, this application provides an intuitive interface for viewing and organizing departments, roles, and employees within the company.\n\nAs a business owner, you can easily navigate through various options to perform essential tasks such as viewing all departments, roles, and employees, adding new departments, roles, and employees, updating employee roles, and deleting employees. This Employee Tracker is an indispensable tool for managing and organizing your company\'s workforce.\n\nThe SQL Challenge: Employee Tracker is built using modern JavaScript technologies and follows best practices for code structure and organization. It incorporates command-line interfaces (CLIs) to provide an efficient and user-friendly experience. With the SQL Challenge: Employee Tracker, you can ensure your employee database remains up to date and accessible, saving time and resources for your business.',
+        repo: 'https://github.com/AlexWatabe9/SQL-Challenge-Employee-Tracker',
+        deployed: '',
+        src: apiQuiz,
+        backgroundColor: '#21b1d1ff',
+        toolsUsed: 'Node.js: The application is built using Node.js, which allows you to run JavaScript code on the server-side. Node.js provides a powerful runtime environment and a vast ecosystem of libraries and packages that can be used to build scalable and efficient applications. Inquirer: Inquirer is a popular NPM package for creating command-line interfaces (CLIs) in Node.js. It provides a set of interactive prompts that allow users to easily navigate and interact with the application through the command line. MySQL: The application uses MySQL as the database management system. MySQL is a widely-used relational database system that provides robust features for storing, retrieving, and managing structured data. It allows you to organize your data into tables and perform complex queries to retrieve specific information. Command-line Interface (CLI): The application utilizes a command-line interface to interact with the user. A CLI provides a text-based interface where users can input commands and receive textual outputs. By using a CLI, you can create a user-friendly and efficient way to manage and interact with your application.',
       repo: 'https://github.com/kpetrosky/SQL-Challenge-Employee-Tracker',
       deployed: 'https://watch.screencastify.com/v/m3nkzd9RcgqnyzR2tkQx',
       src: sql,
@@ -34,38 +39,43 @@ export default function Portfolio() {
     {
       id: 3,
       name: "Web APIs Challenge Code Quiz",
-      description: true,
+      description: 'The Web APIs Challenge: Code Quiz is an interactive web application designed to test and assess your knowledge of JavaScript fundamentals. This coding quiz simulates a typical coding assessment that you may encounter during a job interview or as part of a coding boot camp curriculum. The objective of the quiz is to gauge your progress in JavaScript and compare it to your peers.Upon accessing the quiz, you will be presented with a user-friendly interface that is clean, polished, and responsive. The quiz will begin when you click the start button, triggering a timer that will track the duration of the quiz. As soon as the quiz starts, the first question will be displayed', 
       Repo: "https://github.com/kpetrosky/web-apis-challenge-code-quiz",
       deployed: 'https://kpetrosky.github.io/web-apis-challenge-code-quiz/',
       src: apiQuiz,
       backgroundColor: '#ff9474ff',
+      toolsUsed: 'HTML: You would use HTML to create the structure of the quiz, including elements for questions, answer choices, buttons, and any other necessary components.      CSS: CSS would be used to style and design the quiz interface, making it visually appealing, responsive, and polished. You would define layouts, colors, fonts, and other visual aspects using CSS.JavaScript: JavaScript is essential for implementing the interactive and dynamic features of the quiz. You would write JavaScript code to handle user interactions, manage the timer, present questions and answer choices, track the score, and handle game progression based on user input.',
+
     },
     {
       id: 4,
       name: "Express.js-Challenge-Note-Taker",
-      description: true,
+      description: 'The Note Taker challenge requires you to develop a web application called Note Taker that allows users to write and save notes. The front end of the application has already been created, and your task is to build the back end using Express.js. The application should be able to store and retrieve note data from a JSON file.To successfully complete this challenge, you will need to connect the front end and back end of the application and deploy the entire application to Heroku. As a small business owner, the user story for this application is to provide the ability to write and save notes. This feature allows users to organize their thoughts and keep track of tasks that need to be completed.',
       Repo: "https://github.com/kpetrosky/Express.js-Challenge-Note-Taker",
       deployed: 'https://dashboard.heroku.com/apps/frozen-headland-23136',
       src: express,
       backgroundColor: '#e8aeb7ff',
+      toolsUsed:'Express.js: Express.js is a popular web application framework for Node.js. It simplifies the process of building the back end of your Note Taker application by providing a set of functions and middleware to handle routing, HTTP requests, and other server-side operations. Node.js: Node.js is a JavaScript runtime that allows you to run JavaScript code on the server-side. It is the platform on which Express.js runs and provides a range of modules and packages for building server-side applications.    JavaScript: JavaScript is the programming language used for both the front end and back end development of the Note Taker application. You will write JavaScript code to handle user interactions, perform data storage and retrieval, and connect the front-end components with the back end.JSON (JavaScript Object Notation): JSON is a lightweight data interchange format that is commonly used for storing and transmitting data. In the Note Taker application, you will use JSON to save and retrieve note data from a JSON file.Heroku: Heroku is a cloud platform that enables you to deploy and host your web applications. Once you have completed the development of the Note Taker application, you will deploy it to Heroku to make it accessible to users.',
     },
     {
       id: 5,
       name: "Progressive-Web-Applications-PWA-Challenge-Text-Editor",
-      description: true,
+      description: 'Your challenge is to create a Note Taker application that allows users to write and save notes. The application\'s front end has already been developed, and your task is to build the back end using Express.js. You will also need to connect the front end and back end, and finally deploy the entire application to Heroku. The Note Taker application targets small business owners, providing them with a convenient way to write and save notes. This feature enables users to organize their thoughts and keep track of tasks they need to complete.',
       Repo: "https://github.com/kpetrosky/Progressive-Web-Applications-PWA-Challenge-Text-Editor",
       deployed: 'https://dashboard.heroku.com/apps/lit-sands-84115/',
       src: pwa,
       backgroundColor: '#cbc0d3ff',
+      toolsUsed:'Express.js: Express.js is a popular web application framework for Node.js. It simplifies the process of building the back end of your Note Taker application by providing a set of functions and middleware to handle routing, HTTP requests, and other server-side operations. Node.js: Node.js is a JavaScript runtime that allows you to run JavaScript code on the server-side. It is the platform on which Express.js runs and provides a range of modules and packages for building server-side applications.      JavaScript: JavaScript is the programming language used for both front-end and back-end development in the Note Taker application. You will write JavaScript code to handle user interactions, perform data storage and retrieval, and connect the front-end components with the back end. JSON (JavaScript Object Notation): JSON is a lightweight data interchange format that is commonly used for storing and transmitting data. In the Note Taker application, you will use JSON to save and retrieve note data from a JSON file.',
     },
     {
       id: 6,
-      name: "Full stack MVC with handlebars, express, SQL",
-      description: true,
-      Repo: "https://github.com/kpetrosky/MVC-Model-View-Controller-MVC-Challenge-Tech-Blog",
+      name: 'Full stack MVC with handlebars, express, SQL',
+      description: 'In the world of technology, writing about tech concepts, advancements, and new technologies is just as crucial as creating them. Developers often spend time reading and writing technical articles and tutorials to share their expertise and insights. This week\'s challenge is to build a CMS-style blog site, similar to WordPress, specifically designed for developers to publish their blog posts and engage in discussions by commenting on other developers\' posts. You will create this site from scratch and deploy it to Heroku. The application will follow the MVC (Model-View-Controller) architectural pattern, utilizing Handlebars.js as the templating language, Sequelize as the ORM (Object-Relational Mapping) tool, and the express-session npm package for authentication.',
+      repo: 'https://github.com/kpetrosky/MVC-Model-View-Controller-MVC-Challenge-Tech-Blog',
       deployed: 'https://frozen-lake-62666.herokuapp.com/',
       src: mvc,
       backgroundColor: '#a74482ff',
+      toolsUsed: 'Express.js: It is a web application framework for Node.js that provides a robust set of features for building web applications and APIs. Handlebars.js: It is a templating engine that allows for dynamic rendering of HTML templates based on data received from the server. Handlebars.js simplifies the process of generating HTML content.   Sequelize: It is an ORM (Object-Relational Mapping) tool for Node.js that provides an abstraction layer for interacting with relational databases. Sequelize simplifies database operations and offers features such as model definition, data querying, and migration management. express-session: It is a middleware for Express.js that enables session management and authentication. It allows users to stay authenticated and maintain their session state across different requests. Heroku: It is a cloud platform that enables deployment and hosting of web applications. Heroku provides a simple and scalable infrastructure for deploying web applications, making it easy to deploy the CMS-style blog site.'
     },
   ];
 
@@ -83,7 +93,7 @@ export default function Portfolio() {
               <img src={project.src} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title" style={{ fontSize: '24px' }}>{project.name}</h5>
-                <p className="card-text" style={{ fontSize: '18px' }}>This challenge is exciting. Check it out!</p>
+                <p className="card-text" style={{ fontSize: '18px' }}>{project.description}{project.toolsUsed}</p>
                 <a href={project.Repo} target="_blank" className="btn btn-primary me-3">Github Repo</a>
                 <a href={project.deployed} target="_blank" className="btn btn-primary">Deployed Application</a>
               </div>
